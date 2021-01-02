3d2499ec4a30e1450c3cf688e94c52f6f7202cfb
Cameron Y <cameronslatton@gmail.com>
2021-01-02 11:27:02 -0600

    Added two classes

---
+++ ApiCrudUsingGenerics/ApiCrudUsingGenerics/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrudUsingGenerics.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public int Name { get; set; }
+        public int Roll { get; set; }
+        public int Messages { get; set; }
+
+    }
+}
---
+++ ApiCrudUsingGenerics/ApiCrudUsingGenerics/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrudUsingGenerics.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public int Name { get; set; }
+        public int Subject { get; set; }
+        public int Messages { get; set; }
+    }
+}
