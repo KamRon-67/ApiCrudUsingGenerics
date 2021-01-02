740228470169f39c999f58f82975cfc6437599cf
Cameron Y <cameronslatton@gmail.com>
2021-01-02 13:06:26 -0600

    Done

---
+++ ApiCrudUsingGenerics/ApiCrudUsingGenerics/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCrudUsingGenerics.IService;
+using ApiCrudUsingGenerics.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCrudUsingGenerics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : GenericController<Student>
+    {
+        public StudentsController(IGenericService<Student> genericService) : base(genericService)
+        {
+
+        }
+    }
+}
---
+++ ApiCrudUsingGenerics/ApiCrudUsingGenerics/Controllers/TeachersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCrudUsingGenerics.IService;
+using ApiCrudUsingGenerics.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCrudUsingGenerics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : GenericController<Teacher>
+    {
+        public TeachersController(IGenericService<Teacher> genericService) : base(genericService)
+        {
+
+        }
+    }
+}
