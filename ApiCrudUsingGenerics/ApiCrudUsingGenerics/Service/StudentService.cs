f1ce4146d70190523fe614107ee6514664811817
Cameron Y <cameronslatton@gmail.com>
2021-01-02 11:37:44 -0600

    Added services and interface

---
+++ ApiCrudUsingGenerics/ApiCrudUsingGenerics/Service/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using ApiCrudUsingGenerics.IService;
+using ApiCrudUsingGenerics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrudUsingGenerics.Service
+{
+    public class StudentService : IGenericService<Student>
+    {
+        List<Student> IGenericService<Student>.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Student> IGenericService<Student>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Student IGenericService<Student>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Student> IGenericService<Student>.Insert(Student item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
